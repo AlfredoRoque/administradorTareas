66211d3677f68fc16d9cefb72ae0da1b5f114d2b
AlfredoRoque <46386649+AlfredoRoque@users.noreply.github.com>
2021-09-22 15:11:01 -0500

    version 1.0

--- inc/funciones/conexion.php
+++ inc/funciones/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli('localhost','root','','uptask');
+$conn = new mysqli('us-cdbr-east-04.cleardb.com','bbc5fa77b32947','b7634ce8','heroku_47a8dc202069711');
 
 // echo $conn->ping();
 
